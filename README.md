6066a574b641395abb8648a1a36f69f8ce2e405e
sweetNana <dyahnahdya@gmail.com>
2022-09-11 09:17:55 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Ini adalah tugas satu minggu yang gak tau mau dikerjain kaya gimana :)
+# Tugas-PAM-1
+# Tugas-PAM-1
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:login/datakelompok.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+    KelompokPage.tag: (context) => KelompokPage()
+  };
+
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return MaterialApp(
+      title: 'KerKom',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
